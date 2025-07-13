dc438f0834df61793b2f2d5bcee9daedcd9de3ed
Luiz Felipe Vasconcelos Lapenda <luiz.felipe@mv.com.br>
2025-07-13 20:34:08 -0300

    Adicionado botões que redirecionam para outras páginas e criado estilo deles

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
-  app.innerHTML = `<p>Teste</p>`;
+  app.innerHTML = `<p>Bem Vindo Ao Putty Manager, Escolha a opção que deseja realizar no momento!</p>`;
 });
